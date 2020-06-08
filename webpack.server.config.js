00a8216dd9cd44861c005dd67ac1f72638e27ea2
Joao Edmundo (OSV) <3868648@NLHQ3868648-MBP.local>
2020-06-08 11:24:05 +0200

    fix: build

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -5,29 +5,27 @@ module.exports = {
   mode: 'none',
   entry: {
     // This is our Express server
-    server: './server/server.ts'
+    server: './src/server.ts',
   },
   target: 'node',
   resolve: { extensions: ['.ts', '.js'] },
   optimization: {
-    minimize: false
+    minimize: false,
   },
   output: {
     // Puts the output at the root of the dist folder
     path: path.join(__dirname, './build'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader' }
-    ]
+    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
   },
   plugins: [
     new webpack.ContextReplacementPlugin(
       // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?express(\\|\/)(.+)?/,
       path.join(__dirname, 'src'),
-      {}
-    )
-  ]
+      {},
+    ),
+  ],
 };
